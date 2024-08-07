ea18bb38e73f32a3c919b336219b3df8d7e72c74
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-07 11:35:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+console.log(process.env.SECRET_KEY);
+console.log('Hello world')
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
