e4a1dbb6c361fa716631ce6d99939896832ca312
Anoop Wilson <anoopwilson33@yahoo.com>
2024-08-08 07:40:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
-
+import mongoose from 'mongoose';
+import apiRouter from './routes/index.js';
+import { databaseMDB } from './config/mDB.js';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
-console.log(process.env.SECRET_KEY);
-console.log('Hello world')
+//Router redirected to index.js(server)>index.js(router)>index.js(v1)
+// localhost3000:api/v1/(needed routes) this is how url will be.
+app.use('/api',apiRouter )
 
+
+
+databaseMDB(); // Call the function to establish connection
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
